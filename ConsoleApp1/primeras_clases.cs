a3c19ca63235787d266baff5c2a1d7af6e06a893
Tomas Ignacio Sampietro <08SA42645963@campus.economicas.uba.ar>
2024-03-24 21:29:35 -0300

    Actualizacion1Agregar archivos de proyecto.

---
+++ ConsoleApp1/primeras_clases.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace pildoras
+{
+    class codigo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola");
+            Console.WriteLine(7 * 9);
+
+            int edad;
+            edad = 23;
+            Console.WriteLine("Tenes una edad de" + edad);
+            Console.WriteLine($"Tenes una edad de {edad}");
+
+            // Incremeto y decremento:
+            edad++;
+            Console.WriteLine($"Tenes una edad de {edad}");
+
+            //Ejemplo de parse.
+
+            //1. Pedirle al usuario que ingrese un numero.
+            Console.WriteLine("Ingrese un numero");
+            // ReadLine() es un metodo que lee lo que el usuario ingresa por consola.
+            Console.ReadLine(); // El programa se queda a la espera de que el usuario escriba un valor numerico.
+        }
+    }
+}
---
+++ ConsoleApp2/segundas_clases.cs
@@ -0,0 +1,21 @@
+﻿
+//Curso C#. Sintaxis básica V. Vídeo 7
+using System;
+namespace parte3
+{
+    class ejercicio3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero: ");
+            int num1 = int.Parse(Console.ReadLine()); //Readline, se queda esperando a que el usuario escriba un algo por pantalla. - Devuelve str.
+                                                      //Para convertir el string a int, se utiliza parse.
+
+            Console.WriteLine("Ingrese otro numero: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("El resultado es: " + (num1 + num2));
+        }
+
+    }
+}
